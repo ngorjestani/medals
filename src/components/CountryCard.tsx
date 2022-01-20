c43b6b3ea8d67b1b4ba01729f336e397a9df66ae
Nick Gorjestani <nickg2794@hotmail.com>
2022-01-20 16:06:09 -0600

    Initial commit

---
+++ src/components/CountryCard.tsx
@@ -0,0 +1,36 @@
+﻿import {FormEvent, FunctionComponent, useEffect, useState} from "react";
+import {Button, Card, CardActions, CardContent} from "@mui/material";
+import Typography from "@mui/material/Typography";
+
+type ICountryCardProps = {
+}
+
+export const CountryCard : FunctionComponent<ICountryCardProps> = () => {
+    const [name, setName] = useState('USA');
+    const [gold, setGold] = useState(0);
+
+    return (
+        <Card sx={{minWidth: 275}}>
+            <CardContent>
+                <Typography variant='h5'>
+                    {name}
+                </Typography>
+                <Typography variant='h6'>
+                    Gold medals: {gold}
+                </Typography>
+                <CardActions>
+                    <Button
+                        size='small'
+                        color='primary'
+                        onClick={(e) => {
+                            e.preventDefault();
+                            setGold(prevState => prevState + 1);
+                        }}
+                    >
+                        Add Medal
+                    </Button>
+                </CardActions>
+            </CardContent>
+        </Card>
+    );
+}
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,20 @@
+﻿import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import {FunctionComponent} from "react";
+
+export const Navbar: FunctionComponent = () => {
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        Countries
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
+}
